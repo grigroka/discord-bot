220d7ff53fcec88afd574b896fe5718d650ec60c
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-08 13:43:17 +0200

    basic commands

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('./config/config.json');
+const { prefix, token } = require('./config/config.json');
 
 // Init Discord
 const client = new Discord.Client();
@@ -8,11 +8,22 @@ client.once('ready', () => {
   console.log('Ready!');
 });
 
-client.login(config.token);
+client.login(token);
 
 client.on('message', message => {
-  if (message.content === '!ping') {
-    // send back "Pong." to the channel the message was sent in
+  if (message.content === `${prefix}ping`) {
     message.channel.send('Pong.');
+  } else if (message.content.startsWith(`${prefix}beep`)) {
+    message.channel.send(`Boop. ${message}`);
+  } else if (message.content === `${prefix}server`) {
+    message.channel.send(
+      `Server name: ${message.guild.name}\nTotal members: ${
+        message.guild.memberCount
+      }`
+    );
+  } else if (message.content === `${prefix}user-info`) {
+    message.channel.send(
+      `Your username: ${message.author.username}\nYour ID: ${message.author.id}`
+    );
   }
 });
