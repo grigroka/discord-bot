61632ff5b5aed98f46ee255ef8788c2bb866098f
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-08 14:05:01 +0200

    avatar command

--- app.js
+++ app.js
@@ -49,5 +49,19 @@ client.on('message', message => {
     const taggedUser = message.mentions.users.first();
 
     message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  } else if (command === 'avatar') {
+    if (!message.mentions.users.size) {
+      return message.channel.send(
+        `Your avatar: ${message.author.displayAvatarURL}`
+      );
+    }
+
+    const avatarList = message.mentions.users.map(user => {
+      return `${user.username}'s avatar: ${user.displayAvatarURL}`;
+    });
+
+    // send the entire array of strings as a message
+    // by default, discord.js will `.join()` the array with `\n`
+    message.channel.send(avatarList);
   }
 });
